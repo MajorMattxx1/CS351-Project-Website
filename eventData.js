28ad42a9cd3450f2e7304fe8063ee989d527bcac
MajorMattxx1 <57693098+MajorMattxx1@users.noreply.github.com>
2021-02-22 18:31:21 -0800

    Updated for HW4

---
+++ eventData.js
@@ -0,0 +1,5 @@
+var events = [{"name": "Personal Cheese Sniffing",
+           "dates": ["anytime"]},
+         {"name": "Communal Cheese Sniffing",
+          "dates": ["Every Saturday at 5pm"]},
+         {"name": "Advanced Communal Cheese Sniffing", "dates": ["Every two Sundays, next is on Feb 21st"]}];
